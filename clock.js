0a30dbba77faed337a4308cdf0c39bfdc1a757f0
Pablo Cortez <cspablocortez@gmail.com>
2023-11-12 15:17:04 -0500

    Update clock.js

---
+++ clock.js
@@ -0,0 +1,14 @@
+const hours   = document.getElementById('hours');
+const minutes = document.getElementById('minutes');
+const seconds = document.getElementById('seconds');
+
+function setTime() {
+    const now = new Date();
+    const formatTime = (time) => (time < 10) ? `0${time}` : time;
+
+    hours.innerText   = formatTime(now.getHours());
+    minutes.innerText = formatTime(now.getMinutes());
+    seconds.innerText = formatTime(now.getSeconds());
+}
+
+window.setInterval(setTime, 1000);
