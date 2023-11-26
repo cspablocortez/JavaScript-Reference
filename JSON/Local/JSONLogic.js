18b744f8c047c08959102c2b61a13973d0d3423c
Pablo Cortez <cspablocortez@gmail.com>
2023-11-26 11:47:05 -0800

    Update JSONLogic.js

--- JSON/Local/JSONLogic.js
+++ JSON/Local/JSONLogic.js
@@ -1,9 +1,29 @@
 async function populate() {
     const requestURL = "https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json"
-
     const request = new Request(requestURL);
-
     const response = await fetch(request);
+    const superHeroes = await response.json();
 
-    console.log(response.json);
+    populateHeader(superHeroes);
+    populateHeroes(superHeroes)
 }
+
+function populateHeader(obj) {
+    const header = document.querySelector('header');
+    const H1 = document.createElement('h1');
+    H1.textContent = obj.squadName;
+    header.appendChild(H1);
+
+    const paragraph = document.createElement('p');
+    paragraph.textContent = `Hometown: ${obj.homeTown} // Formed: ${obj.formed}`;
+    header.appendChild(paragraph);
+}
+
+function populateHeroes(obj) {
+    const section = document.querySelector('section');
+    const heroes = obj.members;
+    console.log(heroes);
+}
+
+
+populate();
