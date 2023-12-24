ccaefc2acae0f61cb6cd976642f45773bedadefd
Pablo Cortez <cspablocortez@gmail.com>
2023-12-24 11:29:52 -0800

    Error badge and dynamic background

--- JSON/External/script.js
+++ JSON/External/script.js
@@ -1,6 +1,6 @@
 async function fetchWeatherData() {
+ 
     console.log("Sending request...");
-
     const location = document.getElementById('searchInput').value;
     const requestURL = "https://api.weatherapi.com/v1/current.json?key=657a30cb69d44cdba9a01524231401&q=" + location;
     const request = new Request(requestURL);
@@ -9,12 +9,11 @@ async function fetchWeatherData() {
     console.log(data);
 
     // Error handling
-
     if (data.error) {
         console.error(`Unable to complete request. ${data.error.message}`);
-        const errorBadge = document.getElementById('error-badge');
-        errorBadge.style.visibility = 'visible';
+        setErrorBadgeTo('visible');
     } else {
+        setErrorBadgeTo('invisible');
         populateWeatherCard(data);
     }
 }
@@ -33,5 +32,25 @@ function populateWeatherCard(obj) {
     forecast.textContent = obj.current.condition.text;
     icon.src = obj.current.condition.icon;
     humidity.textContent = `Humidity: ${obj.current.humidity}%`;
-
 }
+
+function setErrorBadgeTo(visibility) {
+    const errorBadge = document.getElementById('error-badge');
+
+    let v = (visibility == "visible" ? "invisible" : "visible");
+
+    if (errorBadge.classList.contains(v)) {
+        errorBadge.classList.remove(v);
+        errorBadge.classList.add(visibility);
+    } 
+}
+
+function setBackground() {
+    let time = 11;
+
+    if (time < 12) {
+        document.body.classList.add('morning');
+    }
+}
+
+setBackground();
