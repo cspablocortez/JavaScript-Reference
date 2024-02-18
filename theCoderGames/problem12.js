09e4395ecbe709f787fa380cd06e88b079a9a53e
Pablo Cortez <cspablocortez@gmail.com>
2024-02-18 15:18:27 -0500

    Create problem12.js

---
+++ theCoderGames/problem12.js
@@ -0,0 +1,10 @@
+const dictionary = {
+    "one": 1,
+    "two": 2,
+    "three": 3
+}
+
+const list1 = Object.keys(dictionary)
+const list2 = Object.values(dictionary)
+
+console.log(`List 1: ${list1}\nList 2: ${list2}`)
