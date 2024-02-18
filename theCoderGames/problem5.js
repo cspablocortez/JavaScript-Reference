5dc8156be300ff028fa1c246f9c16cf6aded4a9c
Pablo Cortez <cspablocortez@gmail.com>
2024-02-18 14:56:32 -0500

    Update problem5.js

--- theCoderGames/problem5.js
+++ theCoderGames/problem5.js
@@ -1,18 +1,15 @@
-const END = 4
+((END=10) => {
+    const fibonacci = (n) => {
+        if (n == 0 || n == 1) {
+            return 1
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2)
+        }
+    } 
 
-function fibonacci(n) {
-    if (n == 0 || n == 1) {
-        return 1
-    } else {
-        // console.log(`Current iteration: ${n - 1} + ${n - 2}`) 
-        return fibonacci(n - 1) + fibonacci(n - 2)
+    const fibArray = []
+    for (let i = 0; i <= END; i++) {
+        fibArray.push(fibonacci(i))
     }
-}
-
-const fibArray = []
-for (let i = 0; i <= END; i++) {
-    fibArray.push(fibonacci(i))
-}
-
-console.log(fibArray)
-
+    console.log(fibArray)
+})()
