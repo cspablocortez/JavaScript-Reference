e93f4a10aa9f4e297a10f27b597bc8e4a0f9aeb0
Pablo Cortez <cspablocortez@gmail.com>
2024-02-18 15:21:30 -0500

    Create problem10.js

---
+++ theCoderGames/problem10.js
@@ -0,0 +1,4 @@
+const n = 29
+const remainder = n % 12 
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+console.log(months[remainder])
