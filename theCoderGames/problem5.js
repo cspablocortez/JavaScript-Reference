a7ec3653d4efb1e6df2d777990b5ccf274f73dcb
Pablo Cortez <cspablocortez@gmail.com>
2024-02-18 14:59:58 -0500

    Update problem5.js

--- theCoderGames/problem5.js
+++ theCoderGames/problem5.js
@@ -13,3 +13,21 @@
     }
     console.log(fibArray)
 })()
+
+
+// Pre-ES6 
+
+function anonymous(END = 4) {
+    function fibonacci(n) {
+        if (n == 0 || n == 1) {
+            return 1
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2)
+        } 
+    }
+    const fibArray = []
+    for (let i = 0; i <= END; i++) {
+        fibArray.push(fibonacci(i))
+    }
+    console.log(fibArray)
+}
