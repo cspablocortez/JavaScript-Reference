0e01efff2579c04d6cade3c422b1fc192f681b7d
Pablo Cortez <cspablocortez@gmail.com>
2024-04-07 12:26:03 -0700

    Service Worker info + styling

--- PWA-Clock/sw.js
+++ PWA-Clock/sw.js
@@ -12,9 +12,44 @@ const APP_STATIC_RESOURCES = [
 ]
 
 self.addEventListener("install", (e) => {
-    e.waitUntil((async () => {
-        const cache = await caches.open(CACHE_NAME);
-        cache.addAll(APP_STATIC_RESOURCES);
-    })(),
+    console.log('Service Worker: Installed');
+    // e.waitUntil((async () => {
+    //     const cache = await caches.open(CACHE_NAME);
+    //     cache.addAll(APP_STATIC_RESOURCES);
+    // })(),
+    // );
+});
+
+self.addEventListener('activate', (e) => {
+    console.log('Service Worker: Activated');
+    e.waitUntil(
+        (async () => {
+            const names = await caches.keys();
+            await Promise.all(
+                names.map((name) => {
+                    if (name !== CACHE_NAME) {
+                        console.log('Service Worker: Clearing old cache.');
+                        return caches.delete(name);
+                    }
+                }),
+            );
+            await clients.claim();
+        })(),
+    );
+});
+
+self.addEventListener('fetch', (e) => {
+    console.log('Service Worker: Fetching');
+    e.respondWith(
+        fetch(e.request)
+            .then((res) => {
+                const resClone = res.clone();
+                caches
+                    .open(CACHE_NAME)
+                    .then((cache) => {
+                        cache.put(e.request, resClone)
+                    });
+                return res;
+            }).catch((err) => caches.match(e.request).then((res) => res))
     );
 });
