098e42a710f01e5dfc8294d1a433bc2c63b2cf99
Pablo Cortez <57056367+cspablocortez@users.noreply.github.com>
2024-04-28 11:46:47 -0700

    serviceworker + updated paths

---
+++ ExpressApp/public/javascripts/sw.js
@@ -0,0 +1,50 @@
+const VERSION = "v1"
+const CACHE_NAME = `PWA-Clock-${VERSION}`
+const APP_STATIC_RESOURCES = [
+    "/",
+    "index.html",
+    "style/normalize",
+    "style/skeleton.css",
+    "style/style.css",
+    "js/clock.js",
+    "js/darkMode.js",
+    "icons/android-chrome-512x512.png"
+]
+
+self.addEventListener("install", (e) => {
+    console.log('Service Worker: Installed');
+});
+
+self.addEventListener('activate', (e) => {
+    console.log('Service Worker: Activated');
+    e.waitUntil(
+        (async () => {
+            const names = await caches.keys();
+            await Promise.all(
+                names.map((name) => {
+                    if (name !== CACHE_NAME) {
+                        console.log('Service Worker: Clearing old cache.');
+                        return caches.delete(name);
+                    }
+                }),
+            );
+            await clients.claim();
+        })(),
+    );
+});
+
+self.addEventListener('fetch', (e) => {
+    console.log('Service Worker: Fetching');
+    e.respondWith(
+        fetch(e.request)
+            .then((res) => {
+                const resClone = res.clone();
+                caches
+                    .open(CACHE_NAME)
+                    .then((cache) => {
+                        cache.put(e.request, resClone)
+                    });
+                return res;
+            }).catch((err) => caches.match(e.request).then((res) => res))
+    );
+});
