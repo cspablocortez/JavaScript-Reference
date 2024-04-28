69a1e960d47612dce286e4737ebe0fe39bb9d897
Pablo Cortez <57056367+cspablocortez@users.noreply.github.com>
2024-04-28 12:04:07 -0700

    Linking pages

--- ExpressApp/routes/index.js
+++ ExpressApp/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Java' });
+  res.render('index', { title: 'Express.js' });
 });
 
 router.get('/clock', function(req, res, next) {
